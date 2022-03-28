1bb7a8c3d19e4e2d7ca278b7d0e8d5d51f759e85
azylazide <60816921+azylazide@users.noreply.github.com>
2022-03-29 05:39:50 +0800

    differentiating between movements in attack state

--- src/Actors/Player/ActionStates/Neutral.gd
+++ src/Actors/Player/ActionStates/Neutral.gd
@@ -2,5 +2,12 @@ extends "res://src/Actors/Player/ActionStates/Alive.gd"
 
 func state_input(_event: InputEvent) -> void:
 	.state_input(_event)
-	if _event.is_action_pressed("attack"):
+	if _event.is_action_pressed("attack")  and ["Idle","Run","GDash"].has(state_machine.observed_state):
+		print("ground attack")
+		state_machine.switch_states("Attack")
+	if _event.is_action_pressed("attack")  and ["Jump","Fall","ADash"].has(state_machine.observed_state):
+		print("aerial attack")
+		state_machine.switch_states("Attack")
+	if _event.is_action_pressed("attack")  and ["WallCling"].has(state_machine.observed_state):
+		print("wall attack")
 		state_machine.switch_states("Attack")
