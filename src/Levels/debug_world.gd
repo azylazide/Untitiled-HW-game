82c5506546254a4aaeedffe26b8a148f842edc9f
azylazide <60816921+azylazide@users.noreply.github.com>
2022-07-08 03:33:03 +0800

    will not crash when player is freed However removing the area detector causes the failure of checking bounds consider adding signal that player is queued then save current bounds

--- src/Levels/debug_world.gd
+++ src/Levels/debug_world.gd
@@ -2,3 +2,8 @@ extends GameManager
 
 func _ready() -> void:
 	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_kill"):
+		$DebugLevel/Player.queue_free()
+	pass
